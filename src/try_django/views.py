301bd8ce22d234bcb2caf00b8c26b7afb449f16e
martin8767 <martin8767@gmail.com>
2020-10-02 12:01:24 -0300

    34 - django form

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
 
+from .forms import ContactForm
+
+
 def home_page(request):
     my_title = "Hello there..."
     context = {"title": my_title}
@@ -15,7 +18,15 @@ def about_page(request):
     return render(request, "about.html", {"title" : "About uss"})
 
 def contact_page(request):
-    return render(request, "hello_world.html", {"title" : "Contact us"})
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = ContactForm()
+    context = {
+        "title" : "Contact us",
+        "form": form
+    }
+    return render(request, "form.html", context)
 
 def example_page(request):
     context = {"title": "Example"}
