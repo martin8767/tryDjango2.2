05961eb235c589ac197b56087da53160a43fb0c7
martin8767 <martin8767@gmail.com>
2020-10-02 00:31:30 -0300

    unique slug

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import BlogPost
 
-def blog_post_detail_page(request, post_id):
-    obj = get_object_or_404(BlogPost, id=post_id)
+def blog_post_detail_page(request, slug):
+    # queryset = BlogPost.objects.filter(slug=slug)
+    # if queryset.count() == 0:
+    #     raise Http404
+    # obj = queryset.first()
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
